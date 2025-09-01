a9edb0d0706439cc2ab7a80a45ccc9af123f74a0
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-09-01 18:34:12 -0400

    Creación del hook para enviar Push Notifications

--- app/index.tsx
+++ app/index.tsx
@@ -1,156 +1,21 @@
-import Constants from "expo-constants";
-import * as Device from "expo-device";
-import * as Notifications from "expo-notifications";
-import { useEffect, useState } from "react";
-import { Button, Platform, Text, View } from "react-native";
+import { ThemedText } from "@/components/ThemedText";
+import { usePushNotifications } from "@/hooks/usePushNotifications";
+import React from "react";
+import { View } from "react-native";
 
-/*
-https://onesignal.com/ para enviar notificaciones push ilimitadas a tus usuarios, expo solo permite 600 notificaciones por segundo
+const PushApp = () => {
 
-Documentación de Expo Notifications:
-https://docs.expo.dev/push-notifications/push-notifications-setup/
+    const { } = usePushNotifications();
 
-Introducción a las compilaciones de desarrollo:
-https://docs.expo.dev/develop/development-builds/introduction/
-
-Documentación de las notificaciones de Expo, en la SDK53, puesto que ahora no se puede hacer notificaciones en Expo Go, ahora hay que hacer build
-https://docs.expo.dev/versions/latest/sdk/notifications/
-
-El ProyectId se obtiene en la página de expo, al crear el proyecto, te da los comandos a ejecutar para obtener el token.
-
-Algo se hará con esto
-https://expo.dev/notifications
-*/
-
-Notifications.setNotificationHandler({
-	handleNotification: async () => ({
-		shouldPlaySound: true,
-		shouldSetBadge: true,
-		shouldShowBanner: true,
-		shouldShowList: true,
-	}),
-});
-
-async function sendPushNotification(expoPushToken: string) {
-	const message = {
-		to: expoPushToken,
-		sound: "default",
-		title: "Original Title",
-		body: "And here is the body!",
-		data: { someData: "goes here" },
-	};
-
-	await fetch("https://exp.host/--/api/v2/push/send", {
-		method: "POST",
-		headers: {
-			Accept: "application/json",
-			"Accept-encoding": "gzip, deflate",
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(message),
-	});
-}
-
-function handleRegistrationError(errorMessage: string) {
-	alert(errorMessage);
-	throw new Error(errorMessage);
-}
-
-async function registerForPushNotificationsAsync() {
-	if (Platform.OS === "android") {
-		await Notifications.setNotificationChannelAsync("default", {
-			name: "default",
-			importance: Notifications.AndroidImportance.MAX,
-			vibrationPattern: [0, 250, 250, 250],
-			lightColor: "#FF231F7C",
-		});
-	}
-
-	if (Device.isDevice) {
-		const { status: existingStatus } =
-			await Notifications.getPermissionsAsync();
-		let finalStatus = existingStatus;
-		if (existingStatus !== "granted") {
-			const { status } = await Notifications.requestPermissionsAsync();
-			finalStatus = status;
-		}
-		if (finalStatus !== "granted") {
-			handleRegistrationError(
-				"Permission not granted to get push token for push notification!"
-			);
-			return;
-		}
-		const projectId =
-			Constants?.expoConfig?.extra?.eas?.projectId ??
-			Constants?.easConfig?.projectId;
-		if (!projectId) {
-			handleRegistrationError("Project ID not found");
-		}
-		try {
-			const pushTokenString = (
-				await Notifications.getExpoPushTokenAsync({
-					projectId,
-				})
-			).data;
-			console.log(pushTokenString);
-			return pushTokenString;
-		} catch (e: unknown) {
-			handleRegistrationError(`${e}`);
-		}
-	} else {
-		handleRegistrationError("Must use physical device for push notifications");
-	}
-}
-
-export default function App() {
-	const [expoPushToken, setExpoPushToken] = useState("");
-	const [notification, setNotification] = useState<
-		Notifications.Notification | undefined
-	>(undefined);
-
-	useEffect(() => {
-		registerForPushNotificationsAsync()
-			.then((token) => setExpoPushToken(token ?? ""))
-			.catch((error: any) => setExpoPushToken(`${error}`));
-
-		const notificationListener = Notifications.addNotificationReceivedListener(
-			(notification) => {
-				setNotification(notification);
-			}
-		);
-
-		const responseListener =
-			Notifications.addNotificationResponseReceivedListener((response) => {
-				console.log(response);
-			});
-
-		return () => {
-			notificationListener.remove();
-			responseListener.remove();
-		};
-	}, []);
-
+    
 	return (
-		<View
-			style={{ flex: 1, alignItems: "center", justifyContent: "space-around" }}
-		>
-			<Text>Your Expo push token: {expoPushToken}</Text>
-			<View style={{ alignItems: "center", justifyContent: "center" }}>
-				<Text>
-					Title: {notification && notification.request.content.title}{" "}
-				</Text>
-				<Text>Body: {notification && notification.request.content.body}</Text>
-				<Text>
-					Data:{" "}
-					{notification && JSON.stringify(notification.request.content.data)}
-				</Text>
-			</View>
-			<Button
-				title="Press to Send Notification"
-				onPress={async () => {
-					await sendPushNotification(expoPushToken);
-				}}
-			/>
+		<View style={{
+            marginHorizontal: 10,
+            marginTop: 5
+        }}>
+			<ThemedText>Token: </ThemedText>
 		</View>
 	);
-}
+};
+
+export default PushApp
---
+++ hooks/usePushNotifications.ts
@@ -0,0 +1,138 @@
+import Constants from "expo-constants";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import { Platform } from "react-native";
+
+/**
+ * Configuración de las notificaciones,
+ * esta configuracion se hace una sola vez y se pasa al objeto
+ */
+Notifications.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowList: true, // Mostrar la lista de notificaciones
+		shouldPlaySound: true, // Reproducir sonido
+		shouldSetBadge: true, // Mostrar el badge en la barra de notificaciones
+		shouldShowBanner: true, // Mostrar la notificación en la barra de notificaciones
+	}),
+});
+
+interface SendPushOptions {
+	to: string[];
+	title: string;
+	body: string;
+	data?: Record<string, any>;
+}
+
+/**
+ * Función para enviar una notificación push
+ * @param options - Objeto con la configuración de la notificación
+ * @param options.to - Array de tokens de Expo Push, cada token representa un usuario
+ * @param options.title - Título de la notificación
+ * @param options.body - Cuerpo o contenido principal de la notificación
+ * @param options.data - Datos adicionales opcionales (no visibles para el usuario)
+ * @returns void
+ */
+async function sendPushNotification(options: SendPushOptions) {
+	const { to, title, body, data } = options;
+
+	const message = {
+		to: to,
+		sound: "default",
+		title: title,
+		body: body,
+		data: data,
+	};
+
+	// URL de la API de Expo para enviar notificaciones push
+	// a los usuarios que tengan el token de expo push
+	await fetch("https://exp.host/--/api/v2/push/send", {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Accept-encoding": "gzip, deflate",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(message),
+	});
+}
+
+/**
+ * Función para manejar errores de registro de notificaciones
+ * @param errorMessage - Mensaje de error a mostrar
+ * @returns void
+ */
+function handleRegistrationError(errorMessage: string) {
+	alert(errorMessage);
+	throw new Error(errorMessage);
+}
+
+/**
+ *
+ */
+async function registerForPushNotificationsAsync() {
+	// Si es android, se configura el canal de notificaciones
+	if (Platform.OS === "android") {
+		await Notifications.setNotificationChannelAsync("default", {
+			name: "default", // Nombre del canal de notificaciones
+			importance: Notifications.AndroidImportance.MAX, // Importancia de la notificación
+			vibrationPattern: [0, 250, 250, 250], // Patrón de vibración
+			lightColor: "#FF231F7C", // Color de la luz
+		});
+	}
+
+	// Si es dispositivo físico, se obtiene el token de expo push
+	if (Device.isDevice) {
+		const { status: existingStatus } =
+			await Notifications.getPermissionsAsync(); // saber sí ya se tiene permiso para enviar notificaciones push
+
+		let finalStatus = existingStatus; // status final de los permisos
+
+		// si no se tiene permiso, se solicita
+		if (existingStatus !== "granted") {
+			// Importante, hay que avisar que vamos a pedir permiso para enviar notificaciones push
+			// con una popup en la app, sí y solo sí el usuario acepta, se le pide el permiso
+			const { status } = await Notifications.requestPermissionsAsync(); // se solicita el permiso para enviar notificaciones push
+			finalStatus = status;
+		}
+
+		// si no se tiene permiso, nada que hacer, no se puede enviar notificaciones push
+		if (finalStatus !== "granted") {
+			handleRegistrationError(
+				"Permission not granted to get push token for push notification!"
+			);
+			return;
+		}
+
+		// se obtiene el projectId de expo
+		const projectId =
+			Constants?.expoConfig?.extra?.eas?.projectId ??
+			Constants?.easConfig?.projectId;
+		if (!projectId) {
+			handleRegistrationError("Project ID not found"); // si no se tiene el projectId, lanza un error
+		}
+
+        // sí tenemos acceso al token
+		try {
+			const pushTokenString = (
+				await Notifications.getExpoPushTokenAsync({
+					projectId,
+				})
+			).data;
+			console.log({[Platform.OS]: pushTokenString});
+			return pushTokenString; // sí tenemos acceso al token, se retorna
+
+		} catch (error: unknown) {
+			handleRegistrationError(`${error}`);
+		}
+
+	} else {
+		handleRegistrationError("Must use physical device for push notifications"); // si no es dispositivo físico, lanza un error
+	}
+}
+
+export const usePushNotifications = () => {
+	return {
+		// Properties
+		// Methods
+	};
+};
